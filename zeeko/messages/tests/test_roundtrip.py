8078597204a84a6a4e45a595ae13f2d6f7095381
Alex Rudy <alex.rudy@gmail.com>
2016-08-24 18:26:51 -0700

    Adding a simple speed test to pytest

---
+++ zeeko/messages/tests/test_roundtrip.py
@@ -0,0 +1,34 @@
+# -*- coding: utf-8 -*-
+import pytest
+import numpy as np
+from zmq.backend.cython.utils import Stopwatch
+
+from ..receiver import ReceivedArray, Receiver
+from ..publisher import PublishedArray, Publisher
+
+def test_roundtrip_speed(push, pull):
+    """Check roundtrip speed"""
+    pub = Publisher()
+    pub['array1'] = np.random.randn(200,200)
+    rec = Receiver()
+    n = 100
+    w = Stopwatch()
+    total = 0.0
+    t = 0.0
+    for i in range(n):
+        w.start()
+        pub.publish(push)
+        pub['array1'] = np.random.randn(200, 200)
+        rec.receive(pull)
+        total += rec['array1'].array[1,1]
+        t += w.stop()
+    
+    total = 0.0
+    to = 0.0
+    for i in range(n):
+        w.start()
+        total += np.random.randn(200, 200)[1,1]
+        to += w.stop()
+    speed = n/float(t-to)
+    
+    assert speed > 0.005
