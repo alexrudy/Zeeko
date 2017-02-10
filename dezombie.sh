7e496005e4059b5c4063277cce6905254a3664c8
Alex Rudy <alex.rudy@gmail.com>
2017-02-10 14:51:40 -0700

    Allow any zombie signal

--- dezombie.sh
+++ dezombie.sh
@@ -1,3 +1,5 @@
 #!/usr/bin/env bash
+SIGNAL=${1:-9}
+echo $SIGNAL
 ps | grep $VIRTUAL_ENV/bin/python | ghead -n -1
-ps | grep $VIRTUAL_ENV/bin/python | ghead -n -1 | awk '{ print $1 }' | xargs kill -9
+ps | grep $VIRTUAL_ENV/bin/python | ghead -n -1 | awk '{ print $1 }' | xargs kill -$SIGNAL
