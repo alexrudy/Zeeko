72d386b911d5743f9d612f4ba579f7f27867bca9
Alex Rudy <alex.rudy@gmail.com>
2017-02-21 14:48:15 -0800

    Up the timeout in test_pipeline

--- zeeko/telemetry/tests/test_pipeline.py
+++ zeeko/telemetry/tests/test_pipeline.py
@@ -32,7 +32,7 @@ def test_run_pipeline(pipeline, Publisher, pub, filename, chunksize):
             Publisher.publish(pub)
             time.sleep(0.1)
         pipeline.write.fired.wait(timeout=0.1)
-    pipeline.state.selected("STOP").wait(timeout=0.1)
+    pipeline.state.selected("STOP").wait(timeout=1.0)
     assert pipeline.write.fired.is_set()
     assert pipeline.record.recorder.chunkcount == 1
     with h5py.File(filename, 'r') as f:
