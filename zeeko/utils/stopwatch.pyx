d0af1e3a67570cc033d73de5d4626e47275c59ec
Alex Rudy <alex.rudy@gmail.com>
2017-02-28 17:03:23 -0800

    Remove old print statements from Zeeko stopwatch

--- zeeko/utils/stopwatch.pyx
+++ zeeko/utils/stopwatch.pyx
@@ -14,7 +14,4 @@ cdef class Stopwatch:
         current_utc_time(&end_time)
         delta_time.tv_nsec = (end_time.tv_nsec - self.start_time.tv_nsec)
         delta_time.tv_sec = (end_time.tv_sec - self.start_time.tv_sec)
-        print(self.start_time.tv_sec, self.start_time.tv_nsec)
-        print(end_time.tv_sec, end_time.tv_nsec)
-        print(delta_time.tv_sec, delta_time.tv_nsec)
         return timespec_to_microseconds(&delta_time) / 1000000
