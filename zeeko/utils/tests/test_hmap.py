a79a3f7c42c858ae42c618479654cd7589de05b9
Alex Rudy <alex.rudy@gmail.com>
2017-05-10 15:16:28 -0700

    Remove unused tests for hash map

--- zeeko/utils/tests/test_hmap.py
+++ zeeko/utils/tests/test_hmap.py
@@ -13,26 +13,4 @@ def items(n):
     """A list of strings."""
     return ["item{0:d}".format(i) for i in range(n)]
 
-@pytest.mark.skip
-def test_hmap(items):
-    """docstring for test"""
-    h = HashMap(10)
-    if len(items):
-        with pytest.raises(KeyError):
-            h[items[0]]
-    
-    for item in items:
-        h.add(item)
-    assert len(h) == len(items)
-    for i, item in enumerate(items):
-        assert h[item] == i
-    
-    assert repr(h) == "HashMap({0!r})".format(items)
-    
-    if len(items):
-        item = items[0]
-    
-        del h[item]
-        assert len(h) == len(items) - 1
-        assert item not in h
-    
+
